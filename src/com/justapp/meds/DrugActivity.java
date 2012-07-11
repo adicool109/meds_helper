75aa2722ae42417a03bcb98b85b6652092b57f2c
freeman1988 <kononov.dmitriy@gmail.com>
2012-07-11 13:52:59 +0600

    Single drug page fixed

--- src/com/justapp/meds/DrugActivity.java
+++ src/com/justapp/meds/DrugActivity.java
@@ -1,7 +1,12 @@
 package com.justapp.meds;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
 import android.webkit.WebView;
 import android.widget.TextView;
 
@@ -17,7 +22,17 @@ public class DrugActivity extends Activity {
         int selectedId = getIntent().getExtras().getInt("id");
 
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_CUSTOM_TITLE);
         setContentView(R.layout.drug_info);
+        getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.window_title);
+
+        final LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View curtain = inflater.inflate(R.layout.curtain, null);
+        curtain.setVisibility(View.VISIBLE);
+        addContentView(curtain, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+
+
         DBHelper myDbHelper = new DBHelper(this);
         myDbHelper = new DBHelper(this);
         try {
@@ -34,8 +49,11 @@ public class DrugActivity extends Activity {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        TextView drugTitle = (TextView) findViewById(R.id.drugName);
-        drugTitle.setText(title);
+        //TextView drugTitle = (TextView) findViewById(R.id.drugName);
+        //drugTitle.setText(title);
+        TextView headerText = (TextView) findViewById(R.id.headerTitle);
+        headerText.setText(title);
+
         WebView drugInfoContainer = (WebView) findViewById(R.id.drugInfoWebView);
         drugInfoContainer.getSettings().setJavaScriptEnabled(true);
         String header = "<!DOCTYPE html>\n" +
@@ -43,7 +61,7 @@ public class DrugActivity extends Activity {
                 "<head>\n" +
                 "    <meta content=\"text/html; charset=utf-8\" http-equiv=\"Content-Type\" />\n" +
                 "</head>\n" +
-                "<body>\n" +
+                "<body style='background-color:#fefdfb'><p style='font-size:5px;'>&nbsp</p>\n" +
                 drugInfo +
                 "</body>\n" +
                 "</html>";
